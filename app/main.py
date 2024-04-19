c416f1102d4ce712de7f03b711fca981f21cd1db
ypeter0625 <ypeter0625@gmail.com>
2024-04-19 15:57:21 +0900

    4/19 수정
    수정

--- app/main.py
+++ app/main.py
@@ -2,14 +2,18 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import requests
+
 app = FastAPI()
 
-name = 'dong hyun'
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    URL = "http://bigdata.kepco.co.kr/openapi/v1/powerUsage/houseAve.do?year=2020&month=11&metroCd=11&cityCd=12&apiKey=76abU171NsT6039587k9RS061dnAIjP871dmDw1h&returnType=json"
 
-@app.post("/items/{item_id}")
-def create(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    contents = requests.get(URL).text
+
+    return { 'message': contents}
+
+@app.get("\home")
+def home():
+    return {"message": "Home!"}
